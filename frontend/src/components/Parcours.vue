7929170c53a9d3921cecc04127442b7a40424c7d
dev-yann <yanndumas54230@gmail.com>
2018-03-21 11:21:06 +0100

    ez

--- frontend/src/components/Parcours.vue
+++ frontend/src/components/Parcours.vue
@@ -20,6 +20,21 @@
       </v-card>
     </v-flex>
 
+        <v-flex xs12 sm6 offset-sm3 v-for="exercice in ex">
+            <v-card>
+                <v-card-title primary-title>
+                    <div>
+                        <h1 class="headline mb-0">{{exercice.title}}</h1>
+                        <div>{{exercice.description}}</div>
+                    </div>
+                </v-card-title>
+                <v-card-actions>
+                    <v-btn flat color="orange">Share</v-btn>
+                    <v-btn flat color="orange">Explore</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-flex>
+
       <!-- Chat -->
        <Chat></Chat>
 
